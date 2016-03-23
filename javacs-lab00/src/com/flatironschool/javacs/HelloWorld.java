8c82025e730a6ce92e8dc4f4dfe8377598e3153b
Millie Yang <my2440@columbia.edu>
2016-03-23 11:57:26 -0400

    Finished lab00

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,10 +2,18 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
+        return Double.parseDouble(System.getProperty("java.specification.version"));
+        
         // I won't work!
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+		if(getVersion() >= 1.7){
+			System.out.println("Suitable version.");
+		}
+		else{
+			System.out.println("unsuitable version.");
+		}
+	// you can test the output of getVersion() here
     }
 }
